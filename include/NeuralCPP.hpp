5124868cc37bbe41b595e91db519649ea832d686
Manitas Bahri <manitas.bhr@gmail.com>
2023-10-27 18:30:46 +0200

    git: initial commit

---
+++ include/NeuralCPP.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file NeuralCPP.hpp
+ * @brief NeuralCPP is a C++ library specialized in the field of machine learning algorithms.
+ *
+ * @author Manitas Bahri (https://github.com/B-Manitas)
+ * @date 2023
+ */
+
+#pragma once
+
+class NeuralCPP
+{
+public:
+    NeuralCPP();
+    ~NeuralCPP();
+};
