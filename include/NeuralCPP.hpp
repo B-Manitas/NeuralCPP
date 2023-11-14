612f68c9308ddc5174166a42b8b5acf60d4c4420
Manitas Bahri <manitas.bhr@gmail.com>
2023-11-14 19:26:30 +0100

    docs: add docstring in NeuralCPP

--- include/NeuralCPP.hpp
+++ include/NeuralCPP.hpp
@@ -1,16 +1,21 @@
 /**
+ * @defgroup NeuralCPP NeuralCPP
  * @file NeuralCPP.hpp
  * @brief NeuralCPP is a C++ library specialized in the field of machine learning algorithms.
  *
  * @author Manitas Bahri (https://github.com/B-Manitas)
- * @date 2023
+ * @date 2023/11
+ * @license MIT License
  */
 
-#pragma once
+#ifndef NEURALCPP_HPP
+#define NEURALCPP_HPP
 
+/**
+ * @brief NeuralCPP is a C++ library specialized in the field of machine learning algorithms.
+ */
 class NeuralCPP
 {
-public:
-    NeuralCPP();
-    ~NeuralCPP();
 };
+
+#endif // NEURALCPP_HPP
